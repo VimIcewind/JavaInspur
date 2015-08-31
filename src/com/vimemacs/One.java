d550c95a175837e02737a298a6e37afe898ac036
VimIcewind <VimIcewind@gmail.com>
2015-08-31 14:50:59 +0800

    Initialize JavaInspur.

---
+++ src/com/vimemacs/One.java
@@ -0,0 +1,16 @@
+package com.vimemacs;
+
+import java.util.Scanner;
+import java.util.List;
+
+/**
+ * Created by hwd on 2015/8/31.
+ */
+public class One {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input your name");
+        String name = sc.nextLine();
+        System.out.println("Hi, " + name + "!");
+    }
+}
